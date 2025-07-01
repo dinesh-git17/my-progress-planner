b6069bc18d29bf64dfa09e2665ea56554bb3deb4
dinesh-git17 <dineshddawo@gmail.com>
2025-06-30 23:47:14 -0400

    more fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import OneSignalInit from "@/components/OneSignalInit"
 import type { ReactNode } from "react"
+import "./globals.css"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
